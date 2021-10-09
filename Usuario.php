ef578fec25ebfccd6ed8ce99b05ccb0b6c40ba3e
Geronimo <geroalde@live.com.ar>
2021-10-09 16:37:44 -0300

    Agregado de clase usuario

---
+++ Usuario.php
@@ -0,0 +1,33 @@
+<?php
+class Usuario {
+
+    protected $id;
+    protected $nombre;
+    protected $apellido;
+    protected $nombre_usuario;
+
+    public function __construct($nombre, $apellido, $nombre_usuario, $id = null) {
+    $this->id = $id;
+    $this->nombre = $nombre;
+    $this->apellido = $apellido;
+    $this->nombre_usuario = $nombre_usuario;
+}
+    public function getId() {
+        return $this->id;
+    } 
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function getNombreUsuario() {
+        return $this->nombre_usuario;
+    }
+    public function getNombre() {
+        return $this->nombre;
+    }
+    public function getApellido() {
+        return $this->apellido;
+    }
+    public function getNombreApellido() {
+        return "$this->nombre $this->apellido";
+    }
+}
